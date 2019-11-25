3571f80891430838a42edf204836e86a88377a2e
yoshito iwaisako <iwaisako@andfactory.co.jp>
2019-11-25 11:14:48 +0900

    カスタムビュー化

---
+++ app/src/main/java/jp/co/yiwaisako/ui_sample/AutoHorizontalScrollImageView.kt
@@ -0,0 +1,80 @@
+package jp.co.yiwaisako.ui_sample
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class AutoHorizontalScrollImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ImageView(context, attrs, defStyleAttr) {
+
+    init {
+        scaleType = ScaleType.MATRIX
+    }
+
+    // スクロールを繰り返す上限回数
+    var maxRepeatCount = 1
+
+    // 1回のスクロール量
+    var scrollValueXForOneTime = 1
+
+    // スクロールした回数
+    private var repeatCount = 0
+
+    // 現在のX位置
+    private var currentX = 0
+
+    // 繰り返すかどうか
+    private val isAutoScroll: Boolean
+        get() {
+            return repeatCount < maxRepeatCount
+        }
+
+    // スクロール方向
+    private var direction: Direction = Direction.TO_RIGHT
+
+    // スクロール量
+    private val scrollValueX: Int
+        get() {
+            return when (direction) {
+                Direction.TO_RIGHT -> {
+                    scrollValueXForOneTime
+                }
+                Direction.TO_LEFT -> {
+                    -scrollValueXForOneTime
+                }
+            }
+        }
+
+    enum class Direction {
+        TO_RIGHT,
+        TO_LEFT
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        if (isAutoScroll) {
+            scrollTo(scrollValueX)
+            // scrollTo()の後に実行してください
+            decideDirectionAfterScrolled()
+        }
+        super.onDraw(canvas)
+    }
+
+    private fun scrollTo(scrollValueX: Int) {
+        currentX += scrollValueX
+        scrollBy(scrollValueX, 0)
+    }
+
+    private fun decideDirectionAfterScrolled() {
+        if (currentX <= 0) {
+            repeatCount++
+            direction = Direction.TO_RIGHT
+        }
+        if (drawable.intrinsicWidth <= currentX + width) {
+            direction = Direction.TO_LEFT
+        }
+    }
+}
--- app/src/main/java/jp/co/yiwaisako/ui_sample/ScrollImagaeViewActivity.kt
+++ app/src/main/java/jp/co/yiwaisako/ui_sample/ScrollImagaeViewActivity.kt
@@ -1,10 +1,7 @@
 package jp.co.yiwaisako.ui_sample
 
 import android.os.Bundle
-import android.os.Handler
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.doOnLayout
 
 
 class ScrollImagaeViewActivity : AppCompatActivity() {
@@ -12,47 +9,5 @@ class ScrollImagaeViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_imagae_view)
-
-        val image = findViewById<ImageView>(R.id.sushi)
-        image.doOnLayout {
-            var repeatCount = 0
-            val drawableWidth = image.drawable.intrinsicWidth
-            val imageWidth = image.width
-            var isScrollLeftToRight = true
-            var currentX = 0
-
-            val handler = Handler()
-            val r = object : Runnable {
-                override fun run() {
-
-                    if (drawableWidth < currentX + imageWidth) {
-                        isScrollLeftToRight = false
-                    }
-
-                    if (currentX < 0) {
-                        repeatCount++
-                        isScrollLeftToRight = true
-                    }
-
-                    if (repeatCount == 2) return
-
-                    val scrollX = getScrollX(isScrollLeftToRight)
-                    currentX += scrollX
-
-                    image.scrollBy(
-                        scrollX,
-                        0
-                    )
-
-                    handler.postDelayed(this, 10)
-                }
-            }
-            handler.post(r)
-        }
-    }
-
-    private fun getScrollX(isScrollLeftToRight: Boolean): Int {
-        val value = 1
-        return if (isScrollLeftToRight) value else -value
     }
 }
